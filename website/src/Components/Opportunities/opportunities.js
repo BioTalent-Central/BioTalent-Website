754a7e5598b4d86b98fc13595dd2c45346af4afc
flarouche3129 <flarouche3129@gmail.com>
2020-08-18 13:52:32 -0400

    Worked on the opp page and tried to fix the icon logo

--- website/src/Components/Opportunities/opportunities.js
+++ website/src/Components/Opportunities/opportunities.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './opportunities.css';
+import JoinUsOpp from './joinUsOpp.js'
+import QuotesOpp from './quotesOpp'
 
 
 function opportunities(){
-    return(
-        <div>
-            Hello world!
-        </div>
+    return (
+        <section className="opportunities-section">
+            <JoinUsOpp />
+            <QuotesOpp />
+        </section>
     )
 }
 
---
+++ website/src/Components/Opportunities/quotesOpp.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col } from 'shards-react'
+import {
+    Card,
+    CardHeader,
+    CardBody
+} from 'shards-react'
+
+export default class quotesOpp extends React.Component{
+    render() {
+        return (
+            <Container className="quotesOppContainer">
+                <Row>
+                    
+                </Row>
+            </Container>
+        )
+    }
+}
