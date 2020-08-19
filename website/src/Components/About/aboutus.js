41de1be9b75c6631c24e7c627ccce67b011251db
flarouche3129 <flarouche3129@gmail.com>
2020-08-19 11:25:29 -0400

    re-worked opp

--- website/src/Components/About/aboutus.js
+++ website/src/Components/About/aboutus.js
@@ -3,7 +3,6 @@ import "./aboutus.css";
 import BioTalentimage from './images/Picture_BioTalent.png'
 import govImage from './images/Canwordmark_colour.png'
 import Jumbotron from 'react-bootstrap/Jumbotron'
-import Container from 'react-bootstrap/Container'
 import HeaderAboutUs from './headerAboutUs'
 import ContentAboutUs from './contentAboutUs'
 
@@ -12,6 +11,17 @@ function aboutus(){
         <section className="aboutus-section">
             <HeaderAboutUs />
             <ContentAboutUs />
+            <Jumbotron fluid>
+                <div className="aboutus-text-container">
+                    <h1 className="aboutusHeaders">Our partners</h1>
+                    <a className="biotalent-link" href="https://www.biotalent.ca/" target="_blank">
+                        <img src={BioTalentimage} className="BTLogoIcon"/>
+                    </a>
+                    <a className="government-link" href="https://horizons.gc.ca/en/home/" target="_blank">
+                        <img src={govImage} className="PolicyHorizonIcon" />
+                    </a>    
+                </div>
+            </Jumbotron>
         </section>
     )
 }
--- website/src/Components/Opportunities/quotesOpp.js
+++ website/src/Components/Opportunities/quotesOpp.js
@@ -1,53 +1,51 @@
 import React from 'react'
 import "shards-ui/dist/css/shards.min.css"
-import { Container, Row, Col } from 'shards-react'
-import {
-    Card,
-    CardBody,
-    CardFooter
-} from "shards-react"
+import Picture from './images/image1.jpg'
 
-export default class quotesOpp extends React.Component{
+export default class contentAboutUs extends React.Component{
     render() {
         return (
-            <section className="oppContainer">
-                <Row>
-                    <Col className="leftContent">
-                        <Card className="oppCard">
-                            <CardBody className="cardBody">
-                                <p className="quote">
-                                    In partnership with BioTalent Canada, our vision is to provide students with a one of a kind co-op experience through the exploration of solutions to real-world problems in the biotechnology sector.
-                                </p>
-                            </CardBody>
-                            <CardFooter className="cardFooter" >Name of person</CardFooter>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="rightContent">
-                        <Card className="oppCard">
-                            <CardBody className="cardBody">
-                                <p className="quote">
-                                    The Coronavirus pandemic was unprecedented and shocking to the whole world, forcing people to remain home in order to ensure their safety. As a result, many lost their jobs or/and found it difficult to find employment, specifically non essential workers, and college and university students. Fortunately, Mads Kaern got in touch with the BioTalent program in order to procure coop students with placements for the summer. Thus, the uOttawa BioTalent project was created.  
-                                </p>
-                            </CardBody>
-                            <CardFooter className="cardFooter">Name of person</CardFooter>
-                        </Card>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="leftContent">
-                        <Card className="oppCard">
-                            <CardBody className="cardBody"> 
-                                <p className="quote">
-                                        The Coronavirus pandemic was unprecedented and shocking to the whole world, forcing people to remain home in order to ensure their safety. As a result, many lost their jobs or/and found it difficult to find employment, specifically non essential workers, and college and university students. Fortunately, Mads Kaern got in touch with the BioTalent program in order to procure coop students with placements for the summer. Thus, the uOttawa BioTalent project was created.  
-                                </p>
-                            </CardBody>
-                            <CardFooter className="cardFooter">Name of person</CardFooter>
-                        </Card>
-                    </Col>
-                </Row>
-            </section>
+            <div className="quotesContainer">
+                <div className="row">
+                    <div className="two columns">
+                        <img src={Picture} style={{width: 100}}/>
+                    </div>
+                    <div className="ten columns">
+                        <p className="quote">
+                        The BioTalent Program is a remarkable learning experience for students. By offering student-led projects, this program gave me the opportunity to develop my leadership and my organization skills. Working with students from different faculties allowed me to acquire a lot of new knowledge and benefit from the different strengths all individuals can bring to a project. I am grateful to the BioTalent Program for giving me such a great work experience.
+                                        Marie-Pier Laberge
+
+                        </p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="ten columns">
+                    <p className="quote">
+                    I participated in the uOttawa BioTalent program during my first CO-OP work term, and it was a phenomenal experience. Not only did I learn how to apply my academic knowledge to the workplace, but I also gained valuable knowledge from different areas of study since I was part of an interdisciplinary student team. For instance, I learned quite a lot about biotechnology, mechanical engineering, business, and entrepreneurship. I further obtained critical skills in project development and management, and I significantly improved my teamwork and leadership skills. Overall, it has been a truly rewarding experience; I am grateful that I was given this opportunity, especially during a worldwide pandemic. My sincere thanks go to Dr. Mads Kaern, the University of Ottawa’s CO-OP Office, and BioTalent Canada, who worked together to provide us with this wonderful learning experience! 
+                                        Sireen Hallal
+
+                        </p>
+                    </div>
+                    <div className="two columns">
+                        <img src={Picture} style={{width: 100}}/>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="two columns">
+                        <img src={Picture} style={{width: 100}}/>
+                    </div>
+
+                    <div className="ten columns">
+                        <p className="quote">
+                        The Biotalent program aids students to apply their current understanding learned from school to an application-based student led project with a team of passionate peers. I enjoyed my work term discovering more about RNA-based biocomputing and how the aspects of engineering and biology come together to modernize computable technology. I would highly recommend this program to any student interested in research project development and curious about the advancement of synthetic biology and the biotechnology industry.
+                                Roy Hwang
+
+                        </p>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
