bdf4584ae7685e95d82c8e377381a99f86570208
flarouche3129 <flarouche3129@gmail.com>
2020-08-20 12:04:42 -0400

    Added a temp link to the join us button

--- website/src/Components/Opportunities/joinUsOpp.js
+++ website/src/Components/Opportunities/joinUsOpp.js
@@ -6,7 +6,7 @@ export default class joinUsOpp extends React.Component {
     render() {
         return (
             <div className="joinUsSection">
-                <a href="#" className="joinUsBtn">Join Us !</a>
+                <a href="https://www.uottawa.ca/financial-aid-awards/work-study-program" className="joinUsBtn">Join Us !</a>
             </div>
         )
     }
