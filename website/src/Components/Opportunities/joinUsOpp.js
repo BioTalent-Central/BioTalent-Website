865dc544a5050604a45da01fff8a56ea11526cd4
flarouche3129 <flarouche3129@gmail.com>
2020-08-20 10:52:13 -0400

    Worked on the join us button

--- website/src/Components/Opportunities/joinUsOpp.js
+++ website/src/Components/Opportunities/joinUsOpp.js
@@ -1,18 +1,13 @@
 import React from 'react'
 import './opportunities.css'
-import { Container, Row, Col } from 'shards-react'
-import {
-    Card,
-    CardHeader,
-    CardBody
-} from 'shards-react'
+
 
 export default class joinUsOpp extends React.Component {
     render() {
         return (
-            <Container>
-                
-            </Container>
+            <div className="joinUsSection">
+                <a href="#" className="joinUsBtn">Join Us !</a>
+            </div>
         )
     }
 }
